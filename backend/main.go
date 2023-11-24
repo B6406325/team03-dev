e17d1af86240690c0cb19b77954e593358d57180
B6406325 <milindafook@gmail.com>
2023-11-24 15:16:43 +0700

    add database

--- backend/main.go
+++ backend/main.go
@@ -5,12 +5,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+const PORT = "8080"
+
 func main() {
 	entity.SetupDatabase()
 	r := gin.Default()
 	r.Use(CORSMiddleware())
 
-	r.Run()
+	r.Run("localhost: " + PORT)
 
 }
 
