26a5d33d5685576cdf4ee1a086900c33208b0fd8
B6406325 <milindafook@gmail.com>
2023-11-25 13:26:21 +0700

    เพิ่มLogin และ Register แต่ยังไม่ทำ Backend

--- frontend/src/page/login/Login.tsx
+++ frontend/src/page/login/Login.tsx
@@ -1,5 +1,75 @@
+import { Link , useNavigate} from 'react-router-dom';
+import './Login.css'
+import { ConfigProvider , Button , Input, Form , message} from 'antd';
+// import { MemberInterface } from '../../interface/Idata';
+// import { ListUsers } from '../../service/http';
+
+
 export default function Login(){
+    const navigate = useNavigate();
+    // const onFinish = async (values: MemberInterface) => {
+    //     let res = await ListUsers(values);
+    //     if (res.message==="Email Not found") {
+    //         message.error("ไม่พบอีเมลดังกล่าว");
+    //     }if (res.message==="invalid password") {
+    //         message.error("รหัสผ่านผิด โปรดลองอีกครั้ง");
+    //     }if (res.message === "Status admin"){
+    //         message.success("สวัสดี แอดมิน");
+    //         setTimeout(function () {
+    //           navigate("/admin");
+    //       }, 2000);
+    //     }
+    //     else {
+    //         if (res.status) {
+    //             message.success("ยินดีต้อนรับ");
+    //             setTimeout(function () {
+    //               navigate("/content");
+    //           }, 2000);
+    //         }
+    //     }
+    // }
+    
     return(
-        <></>
+        <div className='web-login'>
+            <ConfigProvider theme={{
+                components:{
+                    Button:{
+                        colorPrimary: '#F5CE00',
+                        algorithm: true,
+                        primaryColor: '#000000',
+                    },
+                    Input:{
+                        colorPrimary: '#F5CE00',
+                        algorithm: true,
+                    },
+                },
+            }}>
+            <div className='body-login'>
+                <div className='body-login-text'>เข้าสู่ระบบ</div>
+                <Form>
+                    <div className='input-email'>
+                        <Form.Item name="Email" rules={[{required:true, message:"โปรดใส่อีเมล"}]}>
+                            <Input style={{width:670,height:69,fontSize:25,fontFamily:'Mitr'}} placeholder='อีเมล'></Input>
+                        </Form.Item>
+                    </div>
+                    <div className='input-password'>
+                        <Form.Item name="Password" rules={[{required:true, message:"โปรดใส่รหัสผ่าน"}]}>
+                            <Input style={{width:670,height:69,fontSize:25,fontFamily:'Mitr'}} placeholder='รหัสผ่าน' type='password'></Input>
+                        </Form.Item>
+                    </div>
+                    <div className='reg-text'> ยังไม่มีบัญชีผู้ใช้งาน?
+                        <Link to='/register' style={{textDecoration: 'none'}}>
+                            <div className='reg-link'>สร้างบัญชีผู้ใช้งาน</div>
+                        </Link>
+                        <div className='login-button'>
+                            <Form.Item>
+                                <Button style={{fontSize: 25,width: 200,height:50,fontFamily:'Mitr'}} type='primary' htmlType='submit'>เข้าสู่ระบบ</Button>
+                            </Form.Item>
+                        </div>
+                    </div>
+                </Form>
+            </div>
+            </ConfigProvider>
+        </div>
     );
 }
--- frontend/src/page/login/register/Register.tsx
+++ frontend/src/page/login/register/Register.tsx
@@ -1,5 +1,90 @@
+import './Register.css'
+import { ConfigProvider , Button , Input, Form , message} from 'antd';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+// import { MemberInterface } from '../../interface/Idata';
+// import { CreateMember } from '../../service/http';
+
 export default function Register(){
+    const navigate = useNavigate();
+    // const [member, setMember] = useState<MemberInterface[]>([]);
+    // const onFinish = async (values: MemberInterface) => {
+    //     values.Status = "member";
+    //     values.Payment = "ยังไม่จ่าย";
+    //     let res = await CreateMember(values);
+    //     if (res.status){
+    //         message.success("สมัครเสร็จสิ้น โปรดล็อกอินอีกครั้ง");
+    //         setTimeout(function () {
+    //             navigate("/");
+    //         }, 2000);
+    //     }
+    // }
+    function onClickLogin(){
+        navigate("/login");
+    }
+
     return(
-        <></>
+        <ConfigProvider theme={{
+            components:{
+                Button:{
+                    colorPrimary: '#F5CE00',
+                    algorithm: true,
+                    primaryColor: '#000000',
+                },
+                Input:{
+                    colorPrimary: '#F5CE00',
+                    algorithm: true,
+                },
+            },
+        }}>
+        <div className='web-reg'>
+            <div className='body-reg'>
+                <div className='body-reg-text'>สร้างบัญชีผู้ใช้งาน</div>
+                <Form >
+                    <div className='reg-username'>
+                        <Form.Item name="Username" rules={[{required:true, message:"โปรดใส่ชื่อผู้ใช้งาน"}]}>
+                            <Input style={{width:670,height:53,fontSize:25,fontFamily:'Mitr'}} placeholder='ชื่อผู้ใช้งาน'></Input>
+                        </Form.Item>
+                    </div>
+                    <div className='reg-email'>
+                        <Form.Item name="Email" rules={[{required:true, message:"โปรดใส่อีเมล"}]}>
+                            <Input style={{width:670,height:53,fontSize:25,fontFamily:'Mitr'}} placeholder='อีเมล'></Input>
+                        </Form.Item>
+                    </div>
+                    <div className='reg-pass'>
+                        <Form.Item name="Password" hasFeedback rules={[{required:true, message:"โปรดใส่รหัสผ่าน"}]}>
+                            <Input style={{width:670,height:53,fontSize:25,fontFamily:'Mitr'}} placeholder='รหัสผ่าน' type='password'></Input>
+                        </Form.Item>    
+                    </div>
+                    <div className='reg-password'>
+                        <Form.Item name="confirm" dependencies={['Password']} rules={[
+                            {
+                                required:true,
+                                message:"โปรดยืนยันรหัสผ่าน",
+                            },
+                            ({ getFieldValue }) => ({
+                                validator(_, value) {
+                                  if (!value || getFieldValue('Password') === value) {
+                                    return Promise.resolve();
+                                  }
+                                  return Promise.reject(new Error('รหัสผ่านไม่ตรงกัน!'));
+                                },
+                              }),
+                            
+                            ]}>
+                            <Input style={{width:670,height:53,fontSize:25,fontFamily:'Mitr'}} placeholder='ยืนยันรหัสผ่าน' type='password'></Input>
+                        </Form.Item>
+                    </div>
+                    <div className='reg-button'>
+                        <Form.Item>
+                            <Button style={{fontSize: 25,width: 200,height:50,fontFamily:'Mitr'}} type='primary' htmlType='submit'>สร้างบัญชี</Button>
+                        </Form.Item>
+                    </div>
+                </Form>
+            </div>
+        <div className='reg-back-button'><Button style={{fontSize: 25,width: 200,height:50,fontFamily:'Mitr'}} type='primary' onClick={onClickLogin}>ล็อกอิน</Button></div>
+        </div>
+        </ConfigProvider>
+        
     );
 }
