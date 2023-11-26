98fe6cc86ae5af40838691a646cd9afcd8cf61c0
phisit-P <142073093+phisit-P@users.noreply.github.com>
2023-11-26 22:53:12 +0700

    Add path to user profile
    เพิ่มหน้า path สำหรับ user profile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import Register from './page/login/register/Register';
 import Adminhome from './page/fook/adminhome/Adminhome';
 import Movies from './page/fook/movie/Movie';
 import Homepage from './page/note/Homepage/homepage';
+import UserAccount from './page/pool/user_account/userAccount';
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route path='/admin' element={<Adminhome/>}/>
         <Route path='/admin/movie' element={<Movies/>}/>
         <Route path='/homepage' element={<Homepage/>}/>
+        <Route path='/account' element={<UserAccount/>}/>
       </Routes>
     </Router>
     </>
