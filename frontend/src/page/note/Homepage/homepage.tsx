860de14e73d5da3c03c404d775c528d0f4c3d99b
n6teen <note.842545@gmail.com>
2023-12-02 04:10:00 +0700

    Add movieDetailPage

--- frontend/src/page/note/Homepage/homepage.tsx
+++ frontend/src/page/note/Homepage/homepage.tsx
@@ -5,18 +5,19 @@ import './homepage.css';
 import Navbar from '../../../components/navbar';
 
 const movieData = [
-    { title: 'Toy Story 4', duration: '100min', poster: 'https://i.imgur.com/p7WQfVm.jpg' },
-    { title: 'Toy Story 3', duration: '100min', poster: 'https://i.imgur.com/5YUn5L4.jpg' },
-    { title: 'The Hunger Games', duration: '142min', poster: 'https://i.imgur.com/MfZ3ZhJ.jpg' },
-    { title: 'Opphenheimer', duration: '180min', poster: 'https://i.imgur.com/VIQdyDD.jpg' },
-    { title: 'Five Nights At Freddy', duration: '109min', poster: 'https://i.imgur.com/HtX0pTe.jpg' },
-    { title: 'The terminal', duration: '128min', poster: 'https://i.imgur.com/9OSQ1Uy.jpg' },
-    { title: 'Toy Story 4', duration: '100min', poster: 'https://i.imgur.com/p7WQfVm.jpg' },
-    { title: 'Toy Story 3', duration: '100min', poster: 'https://i.imgur.com/5YUn5L4.jpg' },
-    { title: 'The Hunger Games', duration: '142min', poster: 'https://i.imgur.com/MfZ3ZhJ.jpg' },
-    { title: 'Opphenheimer', duration: '180min', poster: 'https://i.imgur.com/VIQdyDD.jpg' },
-    { title: 'Five Nights At Freddy', duration: '109min', poster: 'https://i.imgur.com/HtX0pTe.jpg' },
-    { title: 'The terminal', duration: '128min', poster: 'https://i.imgur.com/9OSQ1Uy.jpg' },
+    { title: 'Toy Story 4', duration: '100min', poster: 'https://i.imgur.com/p7WQfVm.jpg',des: 'When a new toy called \"Forky\" joins Woody and the ang, a road trip alongside old and new friends reveals how big the world can be for a toy.'},
+    { title: 'Toy Story 3', duration: '100min', poster: 'https://i.imgur.com/5YUn5L4.jpg', des: '' },
+    { title: 'The Hunger Games', duration: '142min', poster: 'https://i.imgur.com/MfZ3ZhJ.jpg', des: ''  },
+    { title: 'Opphenheimer', duration: '180min', poster: 'https://i.imgur.com/VIQdyDD.jpg', des: 'เรื่องราวของ Oppenheimer ชายผู้มีปัญหาในตัวเองมากมาย แต่ก็ถูกมองข้ามไปด้วยความปราดเปรื่องของตัวเขา เมื่อเขาถูกขอความช่วยเหลือให้หาหนทางยุติสงครามโลกครั้งที่สอง เขาก็ชี้ไปที่ความหวังเดียวเท่านั้น คือ อาวุธปรมาณูที่มีพลังทำลายล้างรุนแรงจนสามารถยับยั้งไม่ให้ทุกฝ่ายต่อสู้กันต่อไปได้อีก' },
+    { title: 'Five Nights At Freddy', duration: '109min', poster: 'https://i.imgur.com/HtX0pTe.jpg', des: '' },
+    { title: 'The terminal', duration: '128min', poster: 'https://i.imgur.com/9OSQ1Uy.jpg', des: '' },
+    { title: 'Toy Story 4', duration: '100min', poster: 'https://i.imgur.com/p7WQfVm.jpg', des: '' },
+    { title: 'Toy Story 3', duration: '100min', poster: 'https://i.imgur.com/5YUn5L4.jpg', des: '' },
+    { title: 'The Hunger Games', duration: '142min', poster: 'https://i.imgur.com/MfZ3ZhJ.jpg', des: '' },
+    { title: 'Opphenheimer', duration: '180min', poster: 'https://i.imgur.com/VIQdyDD.jpg', des: '' },
+    { title: 'Five Nights At Freddy', duration: '109min', poster: 'https://i.imgur.com/HtX0pTe.jpg' , des: ''},
+    { title: 'The terminal', duration: '128min', poster: 'https://i.imgur.com/9OSQ1Uy.jpg', des: '' },
+       
     
   ];
 const { Meta } = Card;
@@ -54,7 +55,7 @@ export default function Homepage(){
                 <div className='movie-content-section' style={{justifyItems: 'center', alignItems: 'center'}}>
                     <div className='movie-card-container' style={{display:'flex',flexDirection:'row',flexWrap: 'wrap',position:'relative',justifyContent: 'center', alignItems: 'center'}}>
                         {movieData.map((movie, index) => (
-                        <Link to={`/moviedetailpage?title=${encodeURIComponent(movie.title)}&duration=${encodeURIComponent(movie.duration)}&poster=${encodeURIComponent(movie.poster)}`} key={index}>
+                        <Link to={`/moviedetailpage?title=${encodeURIComponent(movie.title)}&duration=${encodeURIComponent(movie.duration)}&poster=${encodeURIComponent(movie.poster)}&des=${encodeURIComponent(movie.des)}`} key={index}>
                             <Card
                                 key={index}
                                 hoverable
